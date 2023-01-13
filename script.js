311236fe36b9910a9ba87f8b0d830fe16273c72c
Monito <phil.menard1668@gmail.com>
2023-01-13 19:58:06 -0300

    Add color to gameboard
    Add color to the gameboard matching with the color of the player who's playing.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const gameboard = document.getElementById('gameboard');
+console.log(gameboard);
+
 const gameBoard = (() => {
     const cells = document.querySelectorAll('.square');
     
@@ -8,35 +11,21 @@ const gameBoard = (() => {
             //CHECK IF CELL IS EMPTY, MARK THE CELL OF THE SIGN OF THE PLAYER AND CHANGE TURN
             if (cells[i].textContent == '') {   
                 if (gameManager.xTurn)  {
-                    cells[i].textContent = 'X';
-                    gameManager.xTurn = false;
-                    gameManager.oTurn = true;
-                    gameManager.count++;
-                    gameManager.endGame();
-                    if (gameManager.count >= 5) {
-                        gameManager.checkAlignment(cells[i], i);
-                    }
+                    gameboard.style.boxShadow = "0px 0px 6px 6px rgb(71, 163, 190)";
+                    playerOne.selectCell(cells, i);
                 }
-                else if (gameManager.oTurn) {
-                    cells[i].textContent = 'O';
-                    gameManager.xTurn = true;
-                    gameManager.oTurn = false;
-                    gameManager.count++;
-                    gameManager.endGame();
-                    if (gameManager.count >= 5) {
-                        gameManager.checkAlignment(cells[i], i);
-                    }
+                else {
+                    gameboard.style.boxShadow = "0px 0px 6px 6px rgb(78, 201, 78)";
+                    playerTwo.selectCell(cells, i);
                 }
-            }
-            
+            }  
         })
     }
-    return{cells};
 })();
 
 const gameManager = (() => {
     let xTurn = true;
-    let oTurn = false;
+    let isPlaying = true;
     let count = 0;
     const possibilities = [['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'], ['0', '3', '6'], 
     ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']];
@@ -47,50 +36,70 @@ const gameManager = (() => {
         }
     };   
 
-    return {xTurn, oTurn, count, endGame, possibilities, checkAlignment};
+    return {xTurn, count, isPlaying, endGame, possibilities};
 })();
 
 const Player = () => {
-
-}
-
-function checkAlignment(cell, index)   {
-    let lastPossibilities = [];
-    //WE ISOLATE THE POSSIBILITES WHICH INCLUDES THE CELL
-    for (let i = 0; i < gameManager.possibilities.length; i++)  {
-        for (let j = 0; j < 3; j++) {
-            if (index == gameManager.possibilities[i][j])   {
-                lastPossibilities.push(gameManager.possibilities[i]);
+    const selectCell = function(cells, index)  {
+        if (gameManager.xTurn)  {
+            cells[index].textContent = 'X';
+            cells[index].style.color = "rgb(42, 137, 42)";
+            gameManager.xTurn = false;
+            gameManager.count++;
+            gameManager.endGame();
+            if (gameManager.count >= 5) {
+                checkAlignment(cells[index], index, cells);
             }
         }
-    }
-    console.log({lastPossibilities});
-
-    //NOW WE COMPARE IF ANY OF LAST POSSIBILITIES GOT THE SAME SIGN THAN CELL
-    for (let i = 0; i < lastPossibilities.length; i++)  {
-        let win = true;
-        for (let j = 0; j < 3; j++) {
-            if (cell.textContent == gameBoard.cells[lastPossibilities[i][j]].textContent)   {
-                console.log('index de la case : ' + lastPossibilities[i][j] + ', case : ' + gameBoard.cells[lastPossibilities[i][j]].textContent);
+        else {
+            cells[index].textContent = 'O';
+            cells[index].style.color = "rgb(28, 107, 130)";
+            gameManager.xTurn = true;
+            gameManager.count++;
+            gameManager.endGame();
+            if (gameManager.count >= 5) {
+                checkAlignment(cells[index], index, cells);
             }
-            else    {
-                win = false;
-                console.log("that doesn't match!");
+        }
+    };
+
+    const  checkAlignment = function(cell, index, cells)   {
+        let lastPossibilities = [];
+        //WE ISOLATE THE POSSIBILITES WHICH INCLUDES THE CELL
+        for (let i = 0; i < gameManager.possibilities.length; i++)  {
+            for (let j = 0; j < 3; j++) {
+                if (index == gameManager.possibilities[i][j])   {
+                    lastPossibilities.push(gameManager.possibilities[i]);
+                }
+            }
+        }
+        console.log({lastPossibilities});
+        
+        //NOW WE COMPARE IF ANY OF LAST POSSIBILITIES GOT THE SAME SIGN THAN CELL
+        for (let i = 0; i < lastPossibilities.length; i++)  {
+            let win = true;
+            for (let j = 0; j < 3; j++) {
+                if (cell.textContent != cells[lastPossibilities[i][j]].textContent)   {
+                    win = false;
+                    break;
+                }
+            }
+    
+            if (win)    {
+                console.log('player wins!');
+                isPlaying = false;
                 break;
             }
         }
-
-        if (win)    {
-            alert('player wins!');
-            break;
-        }
     }
-    
-}
+
+    return  {selectCell};
+};
+
+const playerOne = Player();
+const playerTwo = Player();
 
 /*
-le gamemanager arrête la partie quand un joueur a aligné trois signes
-
 case 1 => 0-1-2 / 0-3-6 / 0-4-8
 case 2 => 0-1-2 / 1-4-7
 case 3 => 0-1-2 / 2-5-8 / 2-4-6
@@ -108,5 +117,7 @@ avec le chiffre égal à l'index de la case sur laquelle le player vient de cliq
 un nouveau tableau temporaire.
 ensuite on refait une boucle avec le nouveau tableau temporaire, et on regarde si une possibilité a 3 signes
 (textContent) identiques. Si c'est le cas, le joueur a gagné, sinon on continue. Si la boucle se termine sans
-que le joueur ait gagné alors c'est le tour du joueur suivant.
+que le joueur ait gagné alors c'est le tour du joueur suivant ou la fin de la partie.
+
+name, button for start/restart, display congratulations
 */
